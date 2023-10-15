a6fe1ee80b161b1de06a715303ccf5ccb1209ac7
LeTienDat05032002 <hieu06122002@gmail.com>
2023-10-15 10:31:34 +0700

    baithuchanh

--- index.js
+++ index.js
@@ -1,64 +1,17 @@
-// console.log("Hello World");
+function doixung(str){
+    if(str < 2){
+        return true;
+    }
+    let changes = 0;
+    let first = 0;
+    let last = str.length -1;
+    while(first < last){
+        if(str[first] != str[last]){
+            changes++;
+        }
+        first++;
+        last--;
+    }
+    return changes;
+}
 
-// let a = 0;
-
-// const B = 1;
-
-// // a = "Hello World";
-// console.log(a + B, a - B, a * B, a / B);
-
-// const myObject = {
-//   id: "1232131",
-//   name: "Hoc Tran",
-// };
-
-// const name = "Hoc Tran";
-
-// //Array
-// const myArray = [];
-// myArray.push("hoctran");
-// myArray.push(1);
-
-// let a = 1;
-
-// let b = a;
-
-// a = 2;
-
-// console.log(b); //1
-
-// let aObject = {
-// //   a: 1,
-// // };
-
-// // let bObject = aObject;
-
-// // aObject.a = 2;
-
-// // console.log(bObject.a); // ?
-
-// let a = 1;
-// let b = 2;
-// if (a == b) {
-//   console.log(true);
-// } else if (a > b) {
-//   console.log("a lon hon b");
-// } else {
-//   console.log(false);
-// }
-
-// for (let i = 0; i < b; i++) {
-//   console.log(i);
-// }
-
-// let myArray = [1, 2, 3, 4];
-// myArray.forEach((element) => {
-//   console.log(element);
-// });
-
-// function tinhTong(a, b) {
-//   return a + b;
-// }
-
-let A = [1, 1, 2, 3, 4]; // true
-let B = [1, 2, 3, 4]; // false
